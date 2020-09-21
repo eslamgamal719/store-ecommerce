0dc87717c8bd70cfa649c345fef616743e52a298
eslamgamal <eslamgamal719a@gmail.com>
2020-09-21 10:09:29 +0200

    end

---
+++ resources/lang/en/admin/tags.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    'main' => 'Main',
+    'tags' => 'tags',
+    'all tags' => 'All Tags',
+    'name' => 'Name',
+    'slug' => 'Slug Name',
+    'operations' => 'Operation',
+    'edit' => 'Edit',
+    'delete' => 'Delete',
+
+    ///////create//////////
+    'add new tag' => 'Add New Tag',
+    'tag data' => 'Tag Data',
+    'create' => 'Create',
+    'update' => 'Update',
+    'return' => 'Return',
+    'name required' => 'Tag Name Is Required',
+    'add successfully' => 'Added Successfully',
+    'fail' => 'There is an error please Try Again',
+    'edit tag' => 'Edit Tag',
+    'tag not found' => 'Tag Is Not Found',
+    'updated successfully' => 'Updated Successfully',
+    'deleted successfully' => 'Deleted Successfully',
+
+    'slug required' => 'Slug Name Is Required',
+    'slug unique' => 'This Slug Name Is Already Exists',
+
+
+
+
+
+
+
+
+];
